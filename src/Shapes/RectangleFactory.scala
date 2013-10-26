3e9a9e58f18e39f56348cec10643e64aa88e43af
goncalo <goncalo@goncalopereira.com>
2013-10-26 18:55:41 +0100

    rectangle to map/flatmap

--- src/Shapes/RectangleFactory.scala
+++ src/Shapes/RectangleFactory.scala
@@ -5,13 +5,13 @@ Flat representation of a Commands.Rectangle made by four lines which overlap in 
 object RectangleFactory {
 	def apply(x: Int, y: Int, x2: Int, y2: Int): Seq[(Int,Int)] = {
 
-		val side1 = LineFactory(x,y,x2,y)
-		val side2 = LineFactory(x,y,x,y2)
-		val side3 = LineFactory(x,y2,x2,y2)
-		val side4 = LineFactory(x2,y,x2,y2)
+	 val sides = List((x,y,x2,y),(x,y,x,y2),(x,y2,x2,y2),(x2,y,x2,y2))
 
-		val sides = side1 ++ side2 ++ side3 ++ side4
 
-		sides.distinct
+		val all = sides.flatMap {
+			case (sX,sY,sX2,sY2) => LineFactory(sX,sY,sX2,sY2)
+		}
+
+		all.distinct
 	}
 }
